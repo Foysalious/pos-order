875cf68eccb5fd704151ed206a5c79c8d27a053b
Abdullah Arnab <abdullah.arnab@sheba.xyz>
2021-03-31 18:37:47 +0600

    ⬆️ Remove Total, Paid Amount and Payment Status Columns
    Type(s): UPDATE
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MSBE-427

--- database/migrations/2021_03_28_055958_create_orders_table.php
+++ database/migrations/2021_03_28_055958_create_orders_table.php
@@ -25,16 +25,12 @@ class CreateOrdersTable extends Migration
             $table->foreign('partner_id')->references('id')->on('partners')
                 ->onUpdate('cascade')->onDelete('set null');
             $table->bigInteger('customer_id')->unsigned()->nullable();
-            $table->foreign('customer_id')->references('id')->on('customers')->onUpdate('cascade')->onDelete('set null');
             $table->enum('status', Statuses::get())->default(Statuses::PENDING);
-            $table->enum('payment_status', PaymentStatuses::get())->nullable();
             $table->bigInteger('sales_channel_id');
             $table->bigInteger('emi_month')->nullable();
             $table->decimal('interest', 11, 2)->nullable();
             $table->decimal('delivery_charge', 11, 2)->nullable();
             $table->decimal('bank_transaction_charge', 11, 2)->nullable();
-            $table->decimal('total_amount', 11, 2)->nullable();
-            $table->decimal('paid_amount', 11, 2)->nullable();
             $table->string('delivery_name');
             $table->string('delivery_mobile');
             $table->string('delivery_address');
