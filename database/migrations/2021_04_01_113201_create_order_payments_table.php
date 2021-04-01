8020682447a991d1c5a6232445500310bc232531
nafeeur <nafeeur@iamsheba.xyz>
2021-04-01 18:01:07 +0600

    ⬆️ Updated Pos_Order_payments DB Migration file
    Type(s): UPDATE
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MSBE-430

---
+++ database/migrations/2021_04_01_113201_create_order_payments_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Services\Transaction\Constants\TransactionTypes;
+
+class CreateOrderPaymentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_payments', function (Blueprint $table) {
+            $table->id();
+
+            $table->bigInteger('order_id')->nullable()->unsigned()->index();
+            $table->foreign('order_id')->references('id')->on('orders')
+                ->onUpdate('cascade')->onDelete('set null');
+            $table->decimal('amount', 11, 2);
+            $table->enum('transaction_type', TransactionTypes::get())->nullable();
+            $table->string('method')->nullable();
+            $table->integer('emi_month')->nullable();
+            $table->decimal('interest', 11, 2);
+
+            commonColumns($table);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_payments');
+    }
+}
