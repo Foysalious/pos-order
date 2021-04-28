6d43d4719d9b53f4d296967cf3953e4b739db234
Ebne Faiz Al-Amin <faiz@iamsheba.xyz>
2021-04-28 12:09:18 +0600

    ⬆️ adding secure_asset method in blade
    Type(s): UPDATE

--- resources/views/vendor/l5-swagger/index.blade.php
+++ resources/views/vendor/l5-swagger/index.blade.php
@@ -5,9 +5,9 @@
   <meta charset="UTF-8">
   <title>{{config('l5-swagger.documentations.'.$documentation.'.api.title')}}</title>
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700|Source+Code+Pro:300,600|Titillium+Web:400,600,700" rel="stylesheet">
-  <link rel="stylesheet" type="text/css" href="{{ l5_swagger_asset($documentation, 'swagger-ui.css') }}" >
-  <link rel="icon" type="image/png" href="{{ l5_swagger_asset($documentation, 'favicon-32x32.png') }}" sizes="32x32" />
-  <link rel="icon" type="image/png" href="{{ l5_swagger_asset($documentation, 'favicon-16x16.png') }}" sizes="16x16" />
+  <link rel="stylesheet" type="text/css" href="{{ secure_asset(l5_swagger_asset($documentation, 'swagger-ui.css')) }}" >
+  <link rel="icon" type="image/png" href="{{ secure_asset(l5_swagger_asset($documentation, 'favicon-32x32.png')) }}" sizes="32x32" />
+  <link rel="icon" type="image/png" href="{{ secure_asset(l5_swagger_asset($documentation, 'favicon-16x16.png')) }}" sizes="16x16" />
   <style>
     html
     {
@@ -67,8 +67,8 @@
 
 <div id="swagger-ui"></div>
 
-<script src="{{ l5_swagger_asset($documentation, 'swagger-ui-bundle.js') }}"> </script>
-<script src="{{ l5_swagger_asset($documentation, 'swagger-ui-standalone-preset.js') }}"> </script>
+<script src="{{ secure_asset(l5_swagger_asset($documentation, 'swagger-ui-bundle.js')) }}"> </script>
+<script src="{{ secure_asset(l5_swagger_asset($documentation, 'swagger-ui-standalone-preset.js')) }}"> </script>
 <script>
 window.onload = function() {
   // Build a system
