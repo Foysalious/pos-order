70dc36b7857a1582f21a1c26f08dcfd1d62dd42f
nafeeur <nafeeur@iamsheba.xyz>
2021-05-02 10:01:45 +0600

    ⬆️ Order review update
    Type(s): UPDATE
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MSBE-544

---
+++ app/Http/Resources/ReviewResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ReviewResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id'                    => $this->id,
+            'product_id'            => $this->product_id,
+            'customer_id'           => $this->customer_id,
+            'order_sku_id'          => $this->order_sku_id,
+            'review_title'          => $this->review_title,
+            'review_details'        => $this->review_details,
+            'rating'                => $this->rating,
+            'category_id'           => $this->category_id,
+            'partner_id'            => $this->partner_id,
+            'images'                => $this->images
+        ];
+    }
+}
