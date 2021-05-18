f1f0274173e2856a725977cc05573d5c9c429753
nafeeur <nafeeur@iamsheba.xyz>
2021-05-18 17:20:03 +0000

    Customer Table Modified

--- database/migrations/2021_05_18_170612_remove_customer_id_to_orders_table.php
+++ database/migrations/2021_05_18_170612_remove_customer_id_to_orders_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCustomersTable extends Migration
+class RemoveCustomerIdToOrdersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,9 @@ class CreateCustomersTable extends Migration
      */
     public function up()
     {
-        Schema::create('customers', function (Blueprint $table) {
-            $table->id();
-            $table->string('name')->index();
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropForeign('orders_customer_id_foreign');
+            $table->dropColumn('customer_id');
         });
     }
 
@@ -26,6 +26,8 @@ class CreateCustomersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('customers');
+        Schema::table('orders', function (Blueprint $table) {
+            //
+        });
     }
 }
