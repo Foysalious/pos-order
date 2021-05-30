7950d28182ec4a08f28e2e18b619fee8bd8acc89
nafeeur <nafeeur@iamsheba.xyz>
2021-05-30 20:46:22 +0600

    ➕ Order Log Creator
    Type(s): ADD
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MSBE-759

--- app/Services/Order/OrderLogCreator.php
+++ app/Services/Order/OrderLogCreator.php
@@ -3,5 +3,21 @@
 
 class orderLogCreator
 {
+    protected $order;
 
+    /**
+     * @param mixed $order
+     * @return orderLogCreator
+     */
+    public function setOrder($order)
+    {
+        $this->order = $order;
+        return $this;
+    }
+
+    public function create()
+    {
+        dd($this->order->items);
+        return 'yes';
+    }
 }
