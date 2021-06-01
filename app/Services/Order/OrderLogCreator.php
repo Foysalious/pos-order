a59827e7b997022c909c621e42c988efa2a7002d
nafeeur <nafeeur@iamsheba.xyz>
2021-06-01 16:42:35 +0600

    ⬆️ Saved order update log
    Type(s): UPDATE
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MSBE-759

--- app/Services/Order/OrderLogCreator.php
+++ app/Services/Order/OrderLogCreator.php
@@ -6,12 +6,32 @@ use Carbon\Carbon;
 
 class OrderLogCreator
 {
-    protected $existingOrder, $changedOrderData, $orderId, $type;
+    protected $existingOrder, $changedOrderData, $orderId, $type, $existingOrderSkus, $newOrderSkus, $orderStatus;
     protected $orderLogRepository;
 
     public function __construct(OrderLogRepositoryInterface $orderLogRepository)
     {
         $this->orderLogRepository = $orderLogRepository;
+    }
+
+    /**
+     * @param mixed $newOrderSkus
+     * @return OrderLogCreator
+     */
+    public function setChangedOrderSkus($newOrderSkus)
+    {
+        $this->newOrderSkus = $newOrderSkus;
+        return $this;
+    }
+
+    /**
+     * @param mixed $orderSkus
+     * @return OrderLogCreator
+     */
+    public function setExistingOrderSkus($orderSkus)
+    {
+        $this->existingOrderSkus = $orderSkus;
+        return $this;
     }
 
     /**
@@ -54,39 +74,52 @@ class OrderLogCreator
         return $this;
     }
 
-    public function create()
+    /**
+     * @param mixed $orderStatus
+     * @return OrderLogCreator
+     */
+    public function setOrderStatus($orderStatus)
     {
-        return $this->orderLogRepository->insert($this->makeLogData());
+        $this->orderStatus = $orderStatus;
+        return $this;
     }
 
-    private function makeLogData()
+    public function create()
+    {
+        return $this->orderLogRepository->insert($this->makeOrderLogData());
+    }
+
+    private function makeOrderLogData()
     {
         $data = [];
-        if(isset($this->orderId)) $data['order_id'] = $this->orderId;
-        if(isset($this->type)) $data['type'] = $this->type;
-        if(isset($this->existingOrder)) $data['old_value'] = $this->makeLogDataToJSON() ?? '';
-        if(isset($this->changedOrderData)) $data['new_value'] = $this->makeLogDataToJSON() ?? '';
+        if(isset($this->orderId)) $data['order_id']             = $this->orderId;
+        if(isset($this->type)) $data['type']                    = $this->type;
+        if(isset($this->existingOrder)) $data['old_value']      = $this->makeLogDataToJSON($this->existingOrder, $this->existingOrderSkus) ?? '';
+        if(isset($this->changedOrderData)) $data['new_value']   = $this->makeLogDataToJSON($this->changedOrderData, $this->newOrderSkus) ?? '';
+        $data['created_by_name']                                = 'anonymous';
+        $data['created_at']                                     = Carbon::now();
         return $data;
     }
 
-    private function makeLogDataToJSON()
+    private function makeLogDataToJSON($order, $skus)
     {
         $data = [];
-        $data['order_id'] = $this->existingOrder->id ?? $this->changedOrderData->id;
-        $data['partner_wise_order_id'] = $this->existingOrder->partner_wise_order_id ?? $this->changedOrderData->partner_wise_order_id;
-        $data['partner_id'] = $this->existingOrder->partner_id ?? $this->changedOrderData->partner_id;
-        $data['customer_id'] = $this->existingOrder->customer_id ?? $this->changedOrderData->customer_id;
-        $data['status'] = $this->existingOrder->status ?? $this->changedOrderData->status;
-        $data['sales_channel_id'] = $this->existingOrder->sales_channel_id ?? $this->changedOrderData->sales_channel_id;
-        $data['emi_month'] = (isset($this->existingOrder->emi_month) ?: "") ?? (isset($this->changedOrderData->emi_month) ?: "");
-        $data['interest'] = (isset($this->existingOrder->interest) ?: "") ?? (isset($this->changedOrderData->interest) ?: "");
-        $data['delivery_charge'] = $this->existingOrder->delivery_charge ?? $this->changedOrderData->delivery_charge;
-        $data['bank_transaction_charge'] = (isset($this->existingOrder->bank_transaction_charge) ?: "") ?? (isset($this->changedOrderData->bank_transaction_charge) ?: "");
-        $data['delivery_name'] = $this->existingOrder->delivery_name ?? $this->changedOrderData->delivery_name;
-        $data['delivery_mobile'] = $this->existingOrder->delivery_mobile ?? $this->changedOrderData->delivery_mobile;
-        $data['delivery_address'] = $this->existingOrder->delivery_address ?? $this->changedOrderData->delivery_address;
-        $data['note'] = (isset($this->existingOrder->note) ?: "") ?? (isset($this->changedOrderData->note) ?: "");
-        $data['voucher_id'] = (isset($this->existingOrder->voucher_id) ?: "") ?? (isset($this->changedOrderData->voucher_id) ?: "");
+        $data['order_id']                   = $order->id;
+        $data['partner_wise_order_id']      = $order->partner_wise_order_id;
+        $data['partner_id']                 = $order->partner_id;
+        $data['customer_id']                = $order->customer_id;
+        $data['status']                     = $order->status;
+        $data['sales_channel_id']           = $order->sales_channel_id;
+        $data['emi_month']                  = isset($order->emi_month) ? $order->emi_month : "";
+        $data['interest']                   = isset($order->interest) ? $order->interest : "";
+        $data['delivery_charge']            = $order->interest->delivery_charge ?? 0.00;
+        $data['bank_transaction_charge']    = isset($order->bank_transaction_charge) ? $order->bank_transaction_charge : "";
+        $data['delivery_name']              = isset($order->delivery_name) ? $order->delivery_name : "";
+        $data['delivery_name']              = isset($order->delivery_mobile) ? $order->delivery_mobile : "";
+        $data['delivery_name']              = isset($order->delivery_address) ? $order->delivery_address : "";
+        $data['note']                       = isset($order->note) ? $order->note : "";
+        $data['voucher_id']                 = isset($order->voucher_id) ? $order->voucher_id : "";
+        $data['products']                   = isset($skus) ? $skus : [];
         return json_encode($data);
     }
 }
