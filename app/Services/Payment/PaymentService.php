23d58afdcf45058d4360d1dadf29b676361bbab7
Abdullah Arnab <abdullah.arnab@sheba.xyz>
2021-06-16 19:18:28 +0600

    ⬆️ Close Pos Order
    Type(s): UPDATE
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MSBE-820

--- app/Services/Payment/PaymentService.php
+++ app/Services/Payment/PaymentService.php
@@ -1,8 +1,10 @@
 <?php namespace App\Services\Payment;
 
 use App\Http\Requests\PaymentRequest;
+use App\Interfaces\OrderRepositoryInterface;
 use App\Services\BaseService;
-use Illuminate\Http\Request;
+use App\Services\Order\PriceCalculation;
+use Carbon\Carbon;
 
 class PaymentService extends BaseService
 {
@@ -11,20 +13,26 @@ class PaymentService extends BaseService
     /**
      * PaymentService constructor.
      * @param Creator $creator
+     * @param OrderRepositoryInterface $orderRepository
      */
-    public function __construct(Creator $creator)
+    public function __construct(Creator $creator, private OrderRepositoryInterface $orderRepository)
     {
         $this->creator = $creator;
     }
 
     public function store(PaymentRequest $request)
     {
-        $this->creator->setOrderId($request->pos_order_id)
+        $payment = $this->creator->setOrderId($request->pos_order_id)
             ->setAmount($request->amount)
             ->setTransactionType($request->transaction_type)
-            ->setMethod($request->method)
+            ->setMethod($request->payment_method)
             ->setEmiMonth($request->emi_month)
             ->setInterest($request->interest)
             ->create();
+        $order = $this->orderRepository->find($request->pos_order_id);
+        /** @var PriceCalculation $priceCalculation */
+        $priceCalculation = app(PriceCalculation::class);
+        $closed_and_paid_at = $payment->created_at ?: Carbon::now();
+        if (!$priceCalculation->getDue() && !$order->closed_and_paid_at) $this->orderRepository->update($order, ['closed_and_paid_at' => $closed_and_paid_at]);
     }
 }
