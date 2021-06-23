ab04931c58d945a651f52d2065397a1efd25c835
shovan <shovan.sheba@gmail.com>
2021-06-23 11:50:59 +0600

    ⬆️ Review list
    Type(s): UPDATE

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -19,8 +19,13 @@ class Review extends Model
         return $this->belongsTo(Customer::class,'customer_id', 'id');
     }
 
+    public function orderSku()
+    {
+        return $this->belongsTo(OrderSku::class,'order_sku_id');
+    }
+
     public function variation()
     {
-        return json_decode('[{"combination":[{"option_id":765,"option_name":"size","option_value_id":1518,"option_value_name":"l","option_value_details":[{"code":"L","type":"size"}]},{"option_id":766,"option_name":"color","option_value_id":1519,"option_value_name":"green","option_value_details":[{"code":"#000111","type":"color"}]}]}]',true);
+        return $this->orderSku->details ? json_decode($this->orderSku->details,true)["combination"] : null;
     }
 }
