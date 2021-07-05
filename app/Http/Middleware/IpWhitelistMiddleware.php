f7c210aa0a9fff8ac385a2707cacb3a5477b284e
Abdullah Arnab <abdullah.arnab@sheba.xyz>
2021-07-05 19:24:32 +0600

    ⬆️ Ip Whitelist Ignore for Local Environment
    Type(s): UPDATE

--- app/Http/Middleware/IpWhitelistMiddleware.php
+++ app/Http/Middleware/IpWhitelistMiddleware.php
@@ -18,8 +18,8 @@ class IpWhitelistMiddleware
     public function handle(Request $request, Closure $next)
     {
         $redis = Redis::connection();
-        if ($redis->exists(config('sheba.whitelisted_ip_redis_key_name')) &&
-            in_array(getIp(), json_decode($redis->get(config('sheba.whitelisted_ip_redis_key_name'))))) {
+        if ((config('app.env') == 'local') || ($redis->exists(config('sheba.whitelisted_ip_redis_key_name')) &&
+                in_array(getIp(), json_decode($redis->get(config('sheba.whitelisted_ip_redis_key_name')))))) {
             return $next($request);
         }
         throw new AuthorizationException;
