2df1c302cdb430a97ffc8ec47e8c712d170c3009
Abdullah Arnab <abdullah.arnab@sheba.xyz>
2021-07-08 15:01:18 +0600

    ⬆️ Order Create Response
    Type(s): UPDATE

--- app/Services/Order/OrderService.php
+++ app/Services/Order/OrderService.php
@@ -105,7 +105,7 @@ class OrderService extends BaseService
 
 //        if ($order) event(new OrderCreated($order));
         if ($request->sales_channel_id == SalesChannelIds::WEBSTORE) dispatch(new OrderPlacePushNotification($order));
-        return $this->success('Successful', ['order' => ['id' => $order->id]]);
+        return $this->success('Successful', null);
     }
 
     public function getOrderDetails($partner_id, $order_id)
