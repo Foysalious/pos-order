43b64fe02b13fe8800ce3f1a9940d83b08c71f56
Abdullah Arnab <abdullah.arnab@sheba.xyz>
2021-07-13 09:39:47 +0600

    ⬆️ Remove Duplicate Code
    Type(s): UPDATE
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MSBE-1005

--- app/Models/OrderSku.php
+++ app/Models/OrderSku.php
@@ -19,48 +19,6 @@ class OrderSku extends BaseModel
         return $this->hasOne(Review::class,'order_sku_id');
     }
 
-    public function calculate()
-    {
-        $this->price = ($this['unit_price'] * $this['quantity']);
-        $this->discountAmount = $this->discount ? (($this->price > $this->discount->amount) ? $this->discount->amount : $this->price) : 0.00;
-        $this->priceAfterDiscount = $this->price - $this->discountAmount;
-        $this->vat = ($this->priceAfterDiscount * $this->vat_percentage) / 100;
-        $this->priceWithVat = $this->price + $this->vat;
-        $this->total = $this->priceWithVat - $this->discountAmount;
-        $this->isCalculated = true;
-        $this->_formatAllToTaka();
-
-        return $this;
-    }
-
-    private function _formatAllToTaka()
-    {
-        $this->price = formatTakaToDecimal($this->price);
-        $this->vat = formatTakaToDecimal($this->vat);
-        $this->priceWithVat = formatTakaToDecimal($this->priceWithVat);
-        $this->discountAmount = formatTakaToDecimal($this->discountAmount);
-        $this->total = formatTakaToDecimal($this->total);
-    }
-    public function getPrice()
-    {
-        return $this->price;
-    }
-
-    public function getVat()
-    {
-        return $this->vat;
-    }
-
-    public function getDiscountAmount()
-    {
-        return $this->discountAmount;
-    }
-
-    public function getTotal()
-    {
-        return $this->total;
-    }
-
     public function discount()
     {
         return $this->hasOne(OrderDiscount::class, 'item_id');
