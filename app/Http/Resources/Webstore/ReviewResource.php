32ad2f383a643dd304e6b617d19a84e85215ce01
shovan <shovan.sheba@gmail.com>
2021-07-15 17:01:12 +0600

    ⬆️ Webstore review
    Type(s): UPDATE
    
    Issue(s):
    - JIRA:
    https://blabol.atlassian.net/browse/MSBE-1031?atlOrigin=eyJpIjoiN2NiOTA5YThmYzc4NDNhMmE2NzI0ZmQzZjhiMmJlN2UiLCJwIjoiaiJ9

---
+++ app/Http/Resources/Webstore/ReviewResource.php
@@ -0,0 +1,35 @@
+<?php namespace App\Http\Resources\Webstore;
+
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ReviewResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id'                    => $this->id,
+            'product_id'            => $this->product_id,
+            'customer_id'           => $this->customer_id,
+            'order_sku_id'          => $this->order_sku_id,
+            'review_title'          => $this->review_title,
+            'review_details'        => $this->review_details,
+            'rating'                => $this->rating,
+            'category_id'           => $this->category_id,
+            'partner_id'            => $this->partner_id,
+            'created_at'            => $this->created_at->format('d-m-Y'),
+            'customer_name'         => $this->customer->name,
+            'images'                => $this->images,
+            'variation'             => $this->variation()
+        ];
+    }
+
+
+}
