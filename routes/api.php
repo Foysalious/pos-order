321d86ee8c96932f3006c69c0820386c3ee31b70
nafeeur <nafeeur@iamsheba.xyz>
2021-07-29 16:38:42 +0600

    ➕ Order list price, due and payment information
    Type(s): ADD
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MSBE-999

--- routes/api.php
+++ routes/api.php
@@ -24,8 +24,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::group(['prefix'=>'v1'], function(){
-    Route::group(['middleware' => 'ip.whitelist'], function ()
-    {
+  //  Route::group(['middleware' => 'ip.whitelist'], function () {
         Route::group(['prefix' => 'customers'], function () {
             Route::post('', [CustomerController::class, 'store']);
             Route::post('/{customer_id}', [CustomerController::class, 'update']);
@@ -58,5 +57,5 @@ Route::group(['prefix'=>'v1'], function(){
         Route::post('customers/{customer}/orders/{order}/review', [ReviewController::class, 'store']);
         Route::get('products/{product}/reviews', [ReviewController::class, 'index']);
         Route::put('partners/{partner_id}',[DataMigrationController::class, 'updatePartnersTable']);
-    });
+  //  });
 });
