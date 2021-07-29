81d7ef5728185bf389b5b9e2552fe2085226535c
nafeeur <nafeeur@iamsheba.xyz>
2021-07-29 13:14:41 +0600

    ➕ Order list price, due and payment information
    Type(s): ADD
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MSBE-999

--- app/Http/Resources/OrderResource.php
+++ app/Http/Resources/OrderResource.php
@@ -43,6 +43,7 @@ class OrderResource extends JsonResource
             'discounted_price' => $price_calculator->getDiscountedPrice(),
             'paid' => $price_calculator->getPaid(),
             'due' => $price_calculator->getDue(),
+            'created_at' => $this->created_at
         ];
     }
 }
