dda76089962c8b7fe36e7c28fdeb7377c68f713e
Ebne Faiz Al-Amin <faiz@iamsheba.xyz>
2021-08-08 20:07:57 +0600

    ⬆️ Order update details send updatable flag
    Type(s): UPDATE
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MSBE-1080

--- app/Services/Order/OrderService.php
+++ app/Services/Order/OrderService.php
@@ -17,9 +17,11 @@ use App\Interfaces\OrderSkusRepositoryInterface;
 use App\Jobs\Order\OrderPlacePushNotification;
 use App\Models\Order;
 use App\Services\BaseService;
+use App\Services\Inventory\InventoryServerClient;
 use App\Services\Order\Constants\OrderLogTypes;
 use App\Services\Order\Constants\SalesChannelIds;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Collection;
 use Illuminate\Validation\ValidationException;
 
 class OrderService extends BaseService
@@ -36,6 +38,7 @@ class OrderService extends BaseService
                                 OrderFilter $orderFilter,
                                 Updater $updater, OrderPaymentRepositoryInterface $orderPaymentRepository,
                                 Creator $creator,
+                                protected InventoryServerClient $client
     )
     {
         $this->orderRepository = $orderRepository;
@@ -115,6 +118,7 @@ class OrderService extends BaseService
         $order = $this->orderRepository->where('partner_id', $partner_id)->find($order_id);
         if (!$order) return $this->error("You're not authorized to access this order", 403);
         $resource = new OrderWithProductResource($order);
+        $resource = $this->addUpdatableFlagForItems($resource,$order);
         return $this->success('Successful', ['order' => $resource], 200);
     }
 
@@ -216,5 +220,33 @@ class OrderService extends BaseService
         $orderPaymentStatus = $this->orderPaymentRepository->where('order_id', $order_id)->get();
         if (count($orderPaymentStatus) > 0) throw new OrderException(trans('order.update.no_customer_update'));
         else return true;
+    }
+
+    private function addUpdatableFlagForItems($order_resource, Order $order)
+    {
+        $order_resource = json_decode(($order_resource->toJson()), true);
+        $sku_ids = $order->orderSkus()->whereNotNull('sku_id')->pluck('sku_id');
+        $sku_details = $this->getSkuDetails($sku_ids, $order);
+        foreach ($order_resource['items'] as &$item) {
+           if ($item['sku_id'] !== null) {
+                $sku = $sku_details->where('id', $item['sku_id'])->first();
+                if ($sku['sku_channel'][0]['price'] != $item['unit_price']){
+                    $item['is_updatable'] = false;
+                } else {
+                    $item['is_updatable'] = true;
+                }
+           } else {
+               $item['is_updatable'] = true;
+           }
+        }
+
+        return $order_resource;
+    }
+
+    private function getSkuDetails($sku_ids, $order)
+    {
+        $url = 'api/v1/partners/' . $order->partner_id . '/skus?skus=' . json_encode($sku_ids->toArray()) . '&channel_id='.$order->sales_channel_id;
+        $sku_details = $this->client->setBaseUrl()->get($url)['skus'] ?? [];
+        return collect($sku_details);
     }
 }
