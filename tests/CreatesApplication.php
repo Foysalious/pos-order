b2e90c1b97dbf49f1e1de5203c2659693d685974
Tisha <tisha@sheba.xyz>
2021-08-25 15:02:22 +0600

    ➕ ✅ Setup class for testing
    Type(s): ADD, TEST
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/SCQA-621

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -3,6 +3,7 @@
 namespace Tests;
 
 use Illuminate\Contracts\Console\Kernel;
+use Illuminate\Foundation\Bootstrap\LoadConfiguration;
 
 trait CreatesApplication
 {
@@ -16,7 +17,11 @@ trait CreatesApplication
         $app = require __DIR__.'/../bootstrap/app.php';
 
         $app->make(Kernel::class)->bootstrap();
-
+        $this->afterApplicationCreated(function () {
+            $this->artisan('config:clear');
+        });
+        (new LoadConfiguration())->bootstrap($app);
+        $this->baseUrl = env('APP_URL');
         return $app;
     }
 }
