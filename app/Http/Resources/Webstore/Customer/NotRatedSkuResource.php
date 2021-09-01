c0579dbe91f1dc6095dc0b40187128e9b4833621
shovan_sheba <shovan.sheba@gmail.com>
2021-09-01 09:37:31 +0600

    ⬆️ Not rated order
    Type(s): UPDATE

--- app/Http/Resources/Webstore/Customer/NotRatedSkuResource.php
+++ app/Http/Resources/Webstore/Customer/NotRatedSkuResource.php
@@ -9,10 +9,11 @@ class NotRatedSkuResource extends JsonResource
     {
         $details = json_decode($this->details,true);
         return [
-            'order_id' => $this->order_id,
+            'sku_id'    =>  $this->id,
             'product_id' => $details ? ($details['product_id'] ?? null): null,
             'product_name' => $details ? ($details['product_name'] ?? null): null,
             'product_image'  =>'https://cdn-shebadev.s3.ap-south-1.amazonaws.com/20210611_233930.jpg',
+            'order_id' => $this->order_id,
             'variation' => $details ? ($details['combination'] ?? null): null,
         ];
     }
