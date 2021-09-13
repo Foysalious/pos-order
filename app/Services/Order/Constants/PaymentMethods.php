2f62823a18630e19893477d93cfca17c2a7a46aa
Abdullah Arnab <abdullah.arnab@sheba.xyz>
2021-09-13 17:51:43 +0600

    ⬆️ Payment Methods
    Type(s): UPDATE

--- app/Services/Order/Constants/PaymentMethods.php
+++ app/Services/Order/Constants/PaymentMethods.php
@@ -7,4 +7,10 @@ class PaymentMethods
     use ConstGetter;
     const PAYMENT_LINK = 'payment_link';
     const CASH_ON_DELIVERY = 'cod';
+    const BKASH = 'bkash';
+    const ONLINE = 'online';
+    const QR_CODE = 'qr_code';
+    const ADVANCE_BALANCE = 'advance_balance';
+    const EMI = 'emi';
+    const OTHERS = 'others';
 }
