3a99033664853069be37bdf7e4a32bba893b4929
Abdullah Arnab <abdullah.arnab@sheba.xyz>
2021-09-16 10:43:55 +0600

    ⬆️ Horizon Start
    Type(s): UPDATE

---
+++ bin/start_horizon.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+. ./bin/parse_env.sh
+
+# shellcheck disable=SC2124
+start_horizon_script="docker exec --user www-data ${CONTAINER_NAME} php artisan horizon"
+eval "${start_horizon_script}"
