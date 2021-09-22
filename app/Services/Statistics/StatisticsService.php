158bb196a30b871798cde18aa095a83e75438a3f
Abdullah Arnab <abdullah.arnab@sheba.xyz>
2021-09-22 18:09:52 +0600

    ⬆️ Order Stats API
    Type(s): UPDATE
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MSBE-840

--- app/Services/Statistics/StatisticsService.php
+++ app/Services/Statistics/StatisticsService.php
@@ -1,12 +1,14 @@
 <?php namespace App\Services\Statistics;
 
 
-use App\Helper\TimeFrame;
 use App\Http\Requests\StatisticsRequest;
 use App\Interfaces\OrderRepositoryInterface;
+use App\Models\Order;
 use App\Services\BaseService;
-use Carbon\Carbon;
-use Illuminate\Http\Request;
+use App\Services\Order\Constants\SalesChannelIds;
+use App\Services\Order\Constants\Statuses;
+use App\Services\Order\PriceCalculation;
+use stdClass;
 
 class StatisticsService extends BaseService
 {
@@ -15,11 +17,24 @@ class StatisticsService extends BaseService
     public function index(StatisticsRequest $request, int $partnerId)
     {
         $timeFrame = makeTimeFrame($request);
-        $statuses = $this->orderRepository->getOrderStatusStatByPartner($partnerId)->map(function ($status) {
-            return [$status->status => $status->count];
+        $statuses = new stdClass();
+        $this->orderRepository->getOrderStatusStatByPartner($partnerId)->each(function ($status) use ($statuses) {
+            $name = $status->status;
+            $statuses->$name = $status->count;
         });
+        $webstore_orders = $this->orderRepository->getOrdersBetweenDatesByPartner($partnerId, $timeFrame, [SalesChannelIds::WEBSTORE]);
+        $webstore_orders->map(function ($webstore_order) {
+            /** @var Order $webstore_order */
+            /** @var PriceCalculation $priceCalculation */
+            $priceCalculation = app(PriceCalculation::class);
+            $webstore_order->sale = $priceCalculation->setOrder($webstore_order)->getDiscountedPrice();
+        });
+        $webstore_sales_count = $webstore_orders->count();
+        $webstore_sales = $webstore_orders->where('status', Statuses::COMPLETED)->sum('sale');
         $statistics = [
-            'status_count' => $statuses
+            'status_count' => $statuses,
+            'total_order' => $webstore_sales_count,
+            'total_sales' => $webstore_sales
         ];
         return $this->success('Successful', ['statistics' => $statistics]);
 
