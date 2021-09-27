0578fa671f20feaa5688f695b495078317fa7344
Abdullah Arnab <abdullah.arnab@sheba.xyz>
2021-09-27 17:55:32 +0600

    ⬆️ App Thumb
    Type(s): UPDATE
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MSBE-1394

--- app/Http/Resources/OrderSkuResource.php
+++ app/Http/Resources/OrderSkuResource.php
@@ -15,10 +15,12 @@ class OrderSkuResource extends JsonResource
     public function toArray($request)
     {
         $sku_details = json_decode($this->details);
+        $default_product_app_thumb = config('s3.url') . "images/pos/services/thumbs/default.jpg";
         return [
             'id' => $this->id,
             'name' => $this->name,
             'sku_id' => $this->sku_id,
+            'app_thumb' => $this->product_image ?: $default_product_app_thumb,
             'combination' => $sku_details->combination ?? null,
             'quantity' => $this->quantity,
             'unit_price' => $this->unit_price,
@@ -27,7 +29,6 @@ class OrderSkuResource extends JsonResource
             'warranty' => $this->warranty,
             'warranty_unit' => $this->warranty_unit,
             'note' => $this->note,
-            'product_image' => $this->product_image,
         ];
     }
 }
