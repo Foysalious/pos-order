b145c8ebb2c78956884085be1d4a3379b99f9f82
Foysal <foysal@iamsheba.xyz>
2021-09-28 12:40:22 +0600

    ⬆️ Payment Link statistics
    Type(s): UPDATE
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MSBE-1349

--- app/Http/Controllers/PaymentLinkController.php
+++ app/Http/Controllers/PaymentLinkController.php
@@ -1,6 +1,7 @@
 <?php namespace App\Http\Controllers;
 
 use App\Services\PaymentLink\PaymentLinkService;
+use App\Services\PaymentLink\PaymentLinkStatistics;
 use Illuminate\Http\Request;
 
 class PaymentLinkController extends Controller
@@ -21,9 +22,9 @@ class PaymentLinkController extends Controller
             'amount' => 'required',
             'purpose' => 'required',
             'customer_id' => 'sometimes|integer|exists:pos_customers,id',
-//            'emi_month'          => 'sometimes|integer|in:' . implode(',', config('emi.valid_months')),
-//            'interest_paid_by'   => 'sometimes|in:' . implode(',', PaymentLinkStatics::paidByTypes()),
-//            'transaction_charge' => 'sometimes|numeric|min:' . PaymentLinkStatics::get_payment_link_commission()
+            'emi_month'          => 'sometimes|integer|in:' . implode(',', config('emi.valid_months')),
+            'interest_paid_by'   => 'sometimes|in:' . implode(',', PaymentLinkStatistics::paidByTypes()),
+            'transaction_charge' => 'sometimes|numeric|min:' . PaymentLinkStatistics::get_payment_link_commission()
         ]);
         return $this->paymentLinkService->store( $request);
     }
