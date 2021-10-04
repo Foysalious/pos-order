f697e55b37f50f7b7ca7c230c137899959e80b93
Ebne Faiz Al-Amin <faiz@sheba.xyz>
2021-10-04 17:31:27 +0600

    ⬆️ Added stock and is_published attr. in order details in item
    Type(s): UPDATE

--- app/Services/Order/OrderService.php
+++ app/Services/Order/OrderService.php
@@ -339,8 +339,11 @@ class OrderService extends BaseService
                 if ($sku['vat_percentage'] != $item['vat_percentage']) {
                     $flag = false;
                 }
+                $item['stock'] = rand(4,10);
+                $item['is_published'] = (bool) rand(0,1);
             }
             $item['is_updatable'] = $flag;
+
         }
         return $order_resource;
     }
