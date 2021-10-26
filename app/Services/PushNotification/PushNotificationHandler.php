5e96b35daa5d8f5be4d6604fddb967499c34b3b7
arnabrahman <arnabrahman@hotmail.com>
2021-10-26 18:52:20 +0600

    ⬆️ Push notification send topic fix
    Type(s): UPDATE
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MSBE-1567

--- app/Services/PushNotification/PushNotificationHandler.php
+++ app/Services/PushNotification/PushNotificationHandler.php
@@ -5,20 +5,18 @@ class PushNotificationHandler
     public function send($topic, $notification = null, $data = null, $priority = null)
     {
         $topicResponse = null;
-        if (config('sheba.send_push_notifications')) {
-            /** @var PushNotificationClient $client */
-            $client = app(PushNotificationClient::class);
-            $data = [
-                "topic" => $topic,
-                "data" => json_encode($data),
-                'channel' => 'firebase'
-            ];
-            if ($notification && $notification['title']) $data['title'] = $notification['title'];
-            if ($notification && $notification['body']) $data['body'] = $notification['body'];
-            if ($priority) $data['priority'] = $priority;
-            $url = 'api/vendors/' . config('sheba.sheba_services_vendor_id') . '/notification/send';
-            $topicResponse = $client->post($url, $data);
-        }
+        /** @var PushNotificationClient $client */
+        $client = app(PushNotificationClient::class);
+        $data = [
+            "topic" => $topic,
+            "data" => json_encode($data),
+            'channel' => 'firebase'
+        ];
+        if ($notification && $notification['title']) $data['title'] = $notification['title'];
+        if ($notification && $notification['body']) $data['body'] = $notification['body'];
+        if ($priority) $data['priority'] = $priority;
+        $url = 'api/vendors/' . config('notification.sheba_services_vendor_id') . '/notification/send';
+        $topicResponse = $client->post($url, $data);
         return $topicResponse;
     }
 }
