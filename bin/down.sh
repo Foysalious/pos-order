9776a76c440f61cb976b8a2c0543fd63c62e07f3
Abdullah Arnab <abdullah.arnab@sheba.xyz>
2021-11-03 09:23:47 +0600

    ⬆️ Revert Down Script
    Type(s): UPDATE

--- bin/down.sh
+++ bin/down.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
+
 . ./bin/parse_env.sh
-docker container rm -f "${CONTAINER_NAME}"
-docker image rm "${CONTAINER_NAME}"
+compose_folder="./docker/composes"
+compose="docker-compose --env-file=./.env -f $compose_folder/docker-compose.yml"
+
+if [[ $1 = "prod" ]]; then
+  extra="-f $compose_folder/docker-compose.prod.yml"
+elif [[ $1 = "dev" ]]; then
+  extra="-f $compose_folder/docker-compose.dev.yml"
+elif [[ $1 = "local" ]]; then
+  extra="-f $compose_folder/docker-compose.local.yml"
+fi
+
+compose="$compose $extra down"
+eval "${compose}"
