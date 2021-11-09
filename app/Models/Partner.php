90912bdf50a94dceaefd0fbfb68681233adc2f2d
Shovan Chowdhury <shovan@sheba.xyz>
2021-11-09 18:54:00 +0600

    resolve partner during order place
    Type(s): UPDATE
    
    Issue(s):
    - JIRA:
    https://blabol.atlassian.net/browse/MSBE-1656?atlOrigin=eyJpIjoiYzA3ODMwYTA1N2M3NDdlNGFmODQwN2MyYTQ4NmVmOWIiLCJwIjoiaiJ9

--- app/Models/Partner.php
+++ app/Models/Partner.php
@@ -4,8 +4,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Partner extends BaseModel
 {
-    protected $guarded = ['id'];
+    protected $guarded = [];
+    protected $primaryKey = 'id';
+    public $incrementing = false;
+
     use HasFactory;
+
     public function orders()
     {
         return $this->hasMany(Order::class);
