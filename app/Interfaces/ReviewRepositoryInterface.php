a4f76f8819702a1bd02b782152ea2d6b0e6b304d
Foysal <foysal@iamsheba.xyz>
2021-11-09 10:34:39 +0600

    ⬆️ Unusesd variable removed
    Type(s): UPDATE
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MSBE-1626

--- app/Interfaces/ReviewRepositoryInterface.php
+++ app/Interfaces/ReviewRepositoryInterface.php
@@ -5,8 +5,11 @@ interface ReviewRepositoryInterface extends BaseRepositoryInterface
     public function createReview($data);
 
     public function getReviews($offset, $limit, $product_id, $rating, $orderBy);
+
     public function getCustomerReviews(string $customer_id, int $offset, int $limit, $order);
+
     public function getCustomerCount(string $customer_id);
+
     public function getProductIdsByRating(int $partnerId, array $ratings);
 
 }
