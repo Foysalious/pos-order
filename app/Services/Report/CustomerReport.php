3d5357d95347694e04bb5607986433c680c57543
Foysal <foysal@iamsheba.xyz>
2021-11-14 17:46:04 +0600

    ⬆️ sales report get
    Type(s): UPDATE
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MSBE-1111

--- app/Services/Report/CustomerReport.php
+++ app/Services/Report/CustomerReport.php
@@ -14,6 +14,7 @@ class CustomerReport
     protected string $from;
     protected string $to;
     protected int $partner_id;
+    private $data = [];
 
     public function __construct(protected OrderRepositoryInterface $orderRepository,)
     {
@@ -89,7 +90,6 @@ class CustomerReport
             ->whereNotNull('customer_id')
             ->whereBetween('created_at', [$this->from, $this->to])
             ->get();
-
         $order_calculator = App::make(PriceCalculation::class);
         $paid = 0;
         $paid_count = 0;
@@ -115,16 +115,23 @@ class CustomerReport
         $data['net_bill'] = $net_bill;
         $data['net_bill_count'] = $order_count;
         $data['order_count'] = $order_count;
-        $data['sales_stat_breakdown'] = $this->getPosOrdersBreakdownData($for = 'sales', $orders, self::MONTH_BASE);
+        $data['sales_stat_breakdown'] = $this->getPosOrdersBreakdownData($order_calculator, $orders, self::MONTH_BASE);
         return $data;
     }
 
-    private function getPosOrdersBreakdownData($for, $pos_orders, $timeline_base)
+    private function getPosOrdersBreakdownData($order_calculator, $order, $timeline_base)
     {
         $is_calculating_for_month = ($timeline_base == self::MONTH_BASE);
-        $pos_orders->each(function ($pos_order) use ($for, $is_calculating_for_month) {
-            $pos_order_created_at_formatter = $is_calculating_for_month ? intval($pos_order->created_at->format('d')) : $pos_order->created_at->format('D');
-            $this->data[$pos_order_created_at_formatter]['amount'] += ($for == 'sales') ? $pos_order->getNetBill() : 1;
+        $order->each(function ($order) use ($order_calculator, $is_calculating_for_month) {
+            $pos_order_created_at_formatter = $is_calculating_for_month ? intval($order->created_at->format('d')) : $order->created_at->format('D');
+            $day = $order->created_at->format('D');
+            $date = $order->created_at->format('d M');
+            if (!array_key_exists($pos_order_created_at_formatter, $this->data))
+                $this->data[$pos_order_created_at_formatter] = ["amount" => 0, "value" => 0, "date" => null, "day" => null];
+            $this->data[$pos_order_created_at_formatter]['amount'] += $order_calculator->getDiscountedPrice();
+            $this->data[$pos_order_created_at_formatter]['value'] += 1;
+            $this->data[$pos_order_created_at_formatter]['date'] = $date;
+            $this->data[$pos_order_created_at_formatter]['day'] = $day;
         });
         return collect($this->data)->values()->all();
     }
