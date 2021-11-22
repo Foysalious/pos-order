83110ba8f204463bec94148f4729dba06f01b928
Shovan Chowdhury <shovan@sheba.xyz>
2021-11-22 10:57:46 +0600

    delivery charge calculation during order place
    Type(s): FIX
    
    Issue(s):
    - JIRA:
    https://blabol.atlassian.net/browse/MSBE-1370?atlOrigin=eyJpIjoiODAzYjRmMDU2OWU4NDg0YWE5NmMzNGJkMGM0NGIzNGUiLCJwIjoiaiJ9

--- app/Services/Order/DeliveryPriceCalculation.php
+++ app/Services/Order/DeliveryPriceCalculation.php
@@ -11,38 +11,7 @@ class DeliveryPriceCalculation
     private $deliveryMethod;
     private Order $order;
 
-    public function calculateDeliveryChargeAndSave(): bool
-    {
-        $this->setDeliveryMethod($this->getDeliveryMethod());
-        if ($this->deliveryMethod == Methods::OWN_DELIVERY && $this->order->deliveryDistrict && $this->order->deliveryThana)
-        {
-            $this->order->delivery_charge = $this->order->partner->delivery_charge;
-            return $this->order->save();
-        }
-        if ($this->order->deliveryDistrict && $this->order->deliveryThana)
-        {
-            $data = [
-                'weight' => $this->order->getWeight(),
-                'delivery_district' => $this->order->deliveryDistrict,
-                'delivery_thana' => $this->order->deliveryThana,
-                'partner_id' => $this->order->partner->id,
-                'cod_amount' => $this->getDue()
-            ];
-            /** @var ApiServerClient $apiServerClient */
-            $apiServerClient = app(ApiServerClient::class);
-            $delivery_charge = $apiServerClient->post('v2/pos/delivery/delivery-charge', $data)['delivery_charge'];
-            $this->order->delivery_charge = $delivery_charge;
-            return $this->order->save();
-        }
-        return false;
-    }
 
-    private function getDeliveryMethod()
-    {
-        /** @var ApiServerClient $apiServerClient */
-        $apiServerClient = app(ApiServerClient::class);
-        return $apiServerClient->get('v1/pos/partners/'. $this->order->partner->id)['partner']['delivery_method'];
-    }
 
     /**
      * @param mixed $deliveryMethod
@@ -72,4 +41,36 @@ class DeliveryPriceCalculation
         return $order_bill->getDue();
     }
 
+    private function getDeliveryMethod()
+    {
+        /** @var ApiServerClient $apiServerClient */
+        $apiServerClient = app(ApiServerClient::class);
+        return $apiServerClient->get('v1/pos/partners/'. $this->order->partner->id)['partner']['delivery_method'];
+    }
+
+    public function calculateDeliveryChargeAndSave(): bool
+    {
+        $this->setDeliveryMethod($this->getDeliveryMethod());
+        if ($this->deliveryMethod == Methods::OWN_DELIVERY && $this->order->deliveryDistrict && $this->order->deliveryThana)
+        {
+            $this->order->delivery_charge = $this->order->partner->delivery_charge;
+            return $this->order->save();
+        }
+        if ($this->order->deliveryDistrict && $this->order->deliveryThana)
+        {
+            $data = [
+                'weight' => $this->order->getWeight(),
+                'delivery_district' => $this->order->deliveryDistrict,
+                'delivery_thana' => $this->order->deliveryThana,
+                'partner_id' => $this->order->partner->id,
+                'cod_amount' => $this->getDue()
+            ];
+            /** @var ApiServerClient $apiServerClient */
+            $apiServerClient = app(ApiServerClient::class);
+            $delivery_charge = $apiServerClient->post('v2/pos/delivery/delivery-charge', $data)['delivery_charge'];
+            $this->order->delivery_charge = $delivery_charge;
+            return $this->order->save();
+        }
+        return false;
+    }
 }
