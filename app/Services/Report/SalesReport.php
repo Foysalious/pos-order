14b72b0606b804eddd5988418fef1a228ddcb736
Foysal <foysal@iamsheba.xyz>
2021-11-22 21:43:52 +0600

    ➕ Sales report fixation
    Type(s): ADD
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MSBE-1111

--- app/Services/Report/SalesReport.php
+++ app/Services/Report/SalesReport.php
@@ -2,6 +2,8 @@
 
 use App\Interfaces\OrderRepositoryInterface;
 use App\Services\Order\PriceCalculation;
+use Carbon\CarbonPeriod;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\App;
 
 class SalesReport
@@ -12,7 +14,7 @@ class SalesReport
     protected int $partner_id;
     private $data = [];
 
-    public function __construct(protected OrderRepositoryInterface $orderRepository,)
+    public function __construct(protected OrderRepositoryInterface $orderRepository, private PriceCalculation $orderCalculation)
     {
     }
 
@@ -53,21 +55,29 @@ class SalesReport
             ->whereNotNull('customer_id')
             ->whereBetween('created_at', [$this->from, $this->to])
             ->get();
-        $order_calculator = App::make(PriceCalculation::class);
         $paid = 0;
         $paid_count = 0;
         $due = 0;
         $due_count = 0;
         $net_bill = 0;
         $order_count = count($orders);
+        $times = CarbonPeriod::create($this->from, $this->to);
+        $time_duration = [];
+        foreach ($times as $time) {
+            $time_duration[$time->format('Y-m-d')] = ["amount" => 0, "orders" => 0, "date" => $time->format('d M'), "day" => $time->format('D')];
+        }
         foreach ($orders as $order) {
-            $order_calculator->setOrder($order);
-            $net_bill = $net_bill + $order_calculator->getDiscountedPrice();
-            $due = $due + $order_calculator->getDue();
-            $paid = $paid + $order_calculator->getPaid();
-            if ($order_calculator->getPaid() > 0)
+            $this->orderCalculation->setOrder($order);
+
+            $time_duration[$order->created_at->toDateString()]['amount']+=$this->orderCalculation->getDiscountedPrice();
+            $time_duration[$order->created_at->toDateString()]['orders']+=1;
+            $order['discounted_price'] = $this->orderCalculation->getDiscountedPrice();
+            $net_bill = $net_bill + $order['discounted_price'];
+            $due = $due + $this->orderCalculation->getDue();
+            $paid = $paid + $this->orderCalculation->getPaid();
+            if ($this->orderCalculation->getPaid() > 0)
                 $paid_count = $paid_count + 1;
-            if ($order_calculator->getDue() > 0)
+            if ($this->orderCalculation->getDue() > 0)
                 $due_count = $due_count + 1;
         }
         $data['paid'] = $paid;
@@ -77,27 +87,12 @@ class SalesReport
         $data['net_bill'] = $net_bill;
         $data['net_bill_count'] = $order_count;
         $data['order_count'] = $order_count;
-        $data['sales_stat_breakdown'] = $this->getPosOrdersBreakdownData($order_calculator, $orders, self::MONTH_BASE);
+        $data['sales_stat_breakdown'] = $time_duration;
         return $data;
     }
 
-    private function getPosOrdersBreakdownData($order_calculator, $order, $timeline_base)
-    {
-        $is_calculating_for_month = ($timeline_base == self::MONTH_BASE);
-        $order->each(function ($order) use ($order_calculator, $is_calculating_for_month) {
-            $pos_order_created_at_formatter = $is_calculating_for_month ? intval($order->created_at->format('d')) : $order->created_at->format('D');
-            $day = $order->created_at->format('D');
-            $date = $order->created_at->format('d M');
-            if (!array_key_exists($pos_order_created_at_formatter, $this->data)){
-                $this->data[$pos_order_created_at_formatter] = ["amount" => 0, "value" => 0, "date" => null, "day" => null];
-            }
-            $this->data[$pos_order_created_at_formatter]['amount'] = $this->data[$pos_order_created_at_formatter]['amount'] + $order_calculator->getDiscountedPrice();
-            $this->data[$pos_order_created_at_formatter]['value'] = $this->data[$pos_order_created_at_formatter]['value'] + 1;
-            $this->data[$pos_order_created_at_formatter]['date'] = $date;
-            $this->data[$pos_order_created_at_formatter]['day'] = $day;
-        });
 
-        return collect($this->data)->values()->all();
-    }
+
+
 
 }
