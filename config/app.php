7c2874de6b1554c29e640d9b63a268695f6149a8
Abdullah Arnab <abdullah.arnab@sheba.xyz>
2021-11-28 13:00:10 +0600

    ⬆️ Clockwork Issue
    Type(s): UPDATE

--- config/app.php
+++ config/app.php
@@ -177,6 +177,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         App\Providers\TelescopeServiceProvider::class,
         App\Providers\RepositoryServiceProvider::class,
+        Clockwork\Support\Laravel\ClockworkServiceProvider::class,
 
     ],
 
