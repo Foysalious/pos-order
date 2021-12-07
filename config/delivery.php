bfaced97d51a13d5c3420a089eb8b5923cb3b936
Shovan Chowdhury <shovan@sheba.xyz>
2021-12-07 18:55:10 +0600

    resolve delivery_vendor
    Type(s): UPDATE
    
    Issue(s):
    - JIRA:
    https://blabol.atlassian.net/browse/MSBE-1877?atlOrigin=eyJpIjoiZDQ5ZWQ1NTZkNjVjNDQwMWJjNTQ5NjU1Y2MxZDYwOWIiLCJwIjoiaiJ9

---
+++ config/delivery.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'own_delivery' => [
+     'name' => 'own_delivery',
+     'image' => null
+    ],
+    'paperfly' => [
+       'name' => 'paperfly',
+       'image' => 'pos/paperfly.png'
+    ]
+];
