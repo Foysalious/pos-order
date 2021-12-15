980726226e817de2ea0e9ddca31c945363878385
Shovan Chowdhury <shovan@sheba.xyz>
2021-12-15 16:07:38 +0600

    env variable name change
    Type(s): UPDATE

--- config/sheba.php
+++ config/sheba.php
@@ -4,7 +4,7 @@ return [
     'whitelisted_ip_redis_key_name' => env('WHITELISTED_IP_REDIS_KEY_NAME', 'WHITELISTED_IPS'),
     'notification_services_app_key' => env('SHEBA_NOTIFICATION_SERVICES_APP_KEY', 'sheba1234'),
     'notification_services_app_secret' => env('SHEBA_NOTIFICATION_SERVICES_APP_SECRET', 'sheba1234'),
-    'api_url' => env('SHEBA_API_URL'),
+    'api_url' => env('SHEBA_NEW_API_URL'),
     'smanager_settings_api_url' => env('SMANAGER_SETTINGS_SERVICE_API_URL', 'https://settings-smanager-webstore.dev-sheba.xyz'),
     'horizon_secret' => env('HORIZON_SECRET'),
     'whitelisted_horizon_ip' => env('WHITELISTED_HORIZON_IP')
