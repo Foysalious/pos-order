eb1f7a228ad5942e96390cc1c640d2de45f8cb42
Arnab Rahman <arnabrahman@hotmail.com>
2022-01-09 12:18:08 +0600

    Event notification table migration

---
+++ database/migrations/2022_01_09_053252_create_event_notifications_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Services\EventNotification\Events;
+use App\Services\EventNotification\Services;
+use App\Services\EventNotification\Statuses;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventNotificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('event_notifications', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')
+                ->constrained()
+                ->restrictOnDelete()
+                ->restrictOnUpdate();
+            $table->enum('service', Services::get())->index();
+            $table->enum('event', Events::get())->index();
+            $table->enum('status', Statuses::get())->index();
+            $table->json('request')->nullable();
+            $table->json('response')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('event_notifications');
+    }
+}
