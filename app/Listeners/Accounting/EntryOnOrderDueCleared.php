155d74e0cc5304b614201d2b3f3ae743e99b162a
Arnab Rahman <arnabrahman@hotmail.com>
2022-01-12 12:53:35 +0600

    Order due cleared job event notification

--- app/Listeners/Accounting/EntryOnOrderDueCleared.php
+++ app/Listeners/Accounting/EntryOnOrderDueCleared.php
@@ -19,16 +19,15 @@ class EntryOnOrderDueCleared
      */
     public function handle(OrderDueCleared|OrderUpdated $event)
     {
-        $event_notification = $this->createEventNotification($event->getOrder(), Events::ORDER_UPDATE);
         if (get_class($event) == OrderUpdated::class) {
             if (empty($event->getOrderProductChangedData()) && !empty($event->getPaymentInfo())) {
                 $paid_amount = $event->getPaymentInfo()['paid_amount'];
                 if (is_null($paid_amount) || $paid_amount == 0) return;
-                $this->dispatch(new OrderDueClearedJob($event->getOrder(), $paid_amount, $event_notification));
+                $this->dispatch(new OrderDueClearedJob($event->getOrder(), $paid_amount, $this->createEventNotification($event->getOrder(), Events::ORDER_UPDATE)));
             }
         } elseif (get_class($event) == OrderDueCleared::class) {
             if (!$event->getPaidAmount() || $event->getPaidAmount() == 0) return;
-            $this->dispatch(new OrderDueClearedJob($event->getOrder(), $event->getPaidAmount(), $event_notification));
+            $this->dispatch(new OrderDueClearedJob($event->getOrder(), $event->getPaidAmount(), $this->createEventNotification($event->getOrder(), Events::ORDER_UPDATE)));
         }
 
     }
