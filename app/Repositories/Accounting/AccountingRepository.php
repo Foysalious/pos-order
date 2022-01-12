09e1ad2f79c632826b65ccde22a9491f52ce19e2
Arnab Rahman <arnabrahman@hotmail.com>
2022-01-12 12:08:54 +0600

    Set event notification in account client

--- app/Repositories/Accounting/AccountingRepository.php
+++ app/Repositories/Accounting/AccountingRepository.php
@@ -2,6 +2,7 @@
 
 use App\Models\Order;
 use App\Repositories\Accounting\Constants\UserType;
+use App\Services\Accounting\AccountingEntryClient;
 use App\Services\ClientServer\Exceptions\BaseClientServerError;
 
 class AccountingRepository extends BaseRepository
@@ -27,7 +28,7 @@ class AccountingRepository extends BaseRepository
      */
     public function storeEntry(int $partner_id, array $data): mixed
     {
-        return $this->client->setUserType(UserType::PARTNER)->setUserId($partner_id)->post("api/entries/", $data);
+        return $this->getClient()->setUserType(UserType::PARTNER)->setUserId($partner_id)->post("api/entries/", $data);
     }
 
     /**
@@ -41,18 +42,23 @@ class AccountingRepository extends BaseRepository
     public function updateEntryBySource(array $data, int $order_id, int $partner_id, string $sourceType = 'pos'): mixed
     {
         $url = "api/entries/source/" . $sourceType . '/' . $order_id;
-        return $this->client->setUserType(UserType::PARTNER)->setUserId($partner_id)
+        return $this->getClient()->setUserType(UserType::PARTNER)->setUserId($partner_id)
             ->post($url, $data);
     }
 
     /**
      * @throws BaseClientServerError
      */
-    public function deleteEntryBySource($source_type,)
+    public function deleteEntryBySource($source_type,): object|array|string|null
     {
         $url = "api/entries/source/" . $source_type . '/' . $this->order->id;
-        return $this->client->setUserType(UserType::PARTNER)->setUserId($this->order->partner_id)
+        return $this->getClient()->setUserType(UserType::PARTNER)->setUserId($this->order->partner_id)
             ->delete($url);
+    }
+
+    private function getClient(): AccountingEntryClient
+    {
+        return $this->client->setEventNotification($this->eventNotification);
     }
 
 }
