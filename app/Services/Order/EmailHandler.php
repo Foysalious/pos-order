5d9a7901f679d65c592e601b1ae3d13fedfee089
Foysal <foysal@iamsheba.xyz>
2022-01-17 15:56:31 +0600

    ➕  email query for customer
    Type(s): UPDATE

--- app/Services/Order/EmailHandler.php
+++ app/Services/Order/EmailHandler.php
@@ -1,5 +1,6 @@
 <?php namespace App\Services\Order;
 
+use App\Models\Customer;
 use App\Models\Order;
 use App\Services\APIServerClient\ApiServerClient;
 use Illuminate\Support\Facades\Mail;
@@ -30,8 +31,10 @@ class EmailHandler
     {
         $partner = $this->client->get('v2/partners/' . $this->order->partner->sub_domain);
         $order_info = $this->orderService->getOrderInfo($this->order->partner_id, $this->order->id);
+        $customer_id = $order_info['customer_id'];
         $order_info = $order_info['items'];
-        Mail::send('emails.pos-order-bill', ['order' => $this->order, 'partner' => $partner, 'order_info' => $order_info], function ($m) {
+        $customer= Customer::where('partner_id',$this->order->partner_id)->where('id',$customer_id)->first();
+        Mail::send('emails.pos-order-bill', ['order' => $this->order, 'partner' => $partner, 'order_info' => $order_info,'customer' => $customer], function ($m) {
             $m->to($this->order->customer->email)->subject('Order Bills');
         });
     }
