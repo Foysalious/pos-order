6d0bf0193f115713d873c2a06478b24d747c52da
Abdullah Arnab <abdullah.arnab@sheba.xyz>
2022-01-27 11:24:17 +0600

    🚑 Duplicate Entry
    Type(s): HOTFIX

--- app/Models/Partner.php
+++ app/Models/Partner.php
@@ -8,6 +8,23 @@ class Partner extends BaseModel
     protected $guarded = [];
     protected $primaryKey = 'id';
     public $incrementing = false;
+    protected $fillable = [
+        'id',
+        'name',
+        'sub_domain',
+        'sms_invoice',
+        'auto_printing',
+        'printer_name',
+        'printer_model',
+        'delivery_charge',
+        'qr_code_account_type',
+        'qr_code_image',
+        'deleted_at',
+        'created_by_name',
+        'updated_by_name',
+        'created_at',
+        'updated_at'
+    ];
 
     use HasFactory,SoftDeletes;
 
