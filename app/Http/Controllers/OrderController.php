cfdcc19792f32cb3893d6dbaefa19e199fabbce6
Abdullah Arnab <abdullah.arnab@sheba.xyz>
2022-03-15 17:32:02 +0600

    🐛 Conditional Invoice Generation in Order Details  For SMS
    Type(s): FIX
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/BBWB1-56

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -202,11 +202,12 @@ class OrderController extends Controller
      *
      * @param $partner_id
      * @param $order_id
+     * @param Request $request
      * @return JsonResponse
      */
-    public function show($partner_id, $order_id): JsonResponse
+    public function show($partner_id, $order_id, Request $request): JsonResponse
     {
-        return $this->orderService->getOrderDetails($partner_id, $order_id);
+        return $this->orderService->getOrderDetails($partner_id, $order_id, $request);
     }
 
     /**
--- app/Services/Order/OrderService.php
+++ app/Services/Order/OrderService.php
@@ -211,10 +211,13 @@ class OrderService extends BaseService
     }
 
 
-    public function getOrderDetails($partner_id, $order_id): JsonResponse
+    public function getOrderDetails($partner_id, $order_id, Request $request): JsonResponse
     {
         $order = $this->orderRepository->getOrderDetailsByPartner($partner_id, $order_id);
         if (!$order) return $this->error("You're not authorized to access this order", 403);
+        if ($request->has('generate_invoice') && $request->generate_invoice == 1) {
+            $this->generateInvoice($order);
+        }
         $resource = new OrderWithProductResource($order, true);
         return $this->success(ResponseMessages::SUCCESS, ['order' => $resource]);
     }
@@ -496,5 +499,18 @@ class OrderService extends BaseService
         $orderIds = !is_array($request->order_ids) ? json_decode($request->order_ids,1) : $request->order_ids;
         $orders = $this->orderRepository->getPartnerWiseOrderIdsFromOrderIds($orderIds, $offset, $limit);
         return $this->success(ResponseMessages::SUCCESS, ['orders' => $orders->keyBy('id')]);
+    }
+
+    /**
+     * @param Order $order
+     */
+    private function generateInvoice(Order $order)
+    {
+        if ($order->invoice == null) {
+            try {
+                app(InvoiceService::class)->setOrder($order)->generateInvoice();
+            } catch (Exception $exception) {
+            }
+        }
     }
 }
