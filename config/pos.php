e717b7d0679e1af6b8bf1cb9ad37789d87e6725f
Abdullah Arnab <abdullah.arnab@sheba.xyz>
2022-06-08 16:43:11 +0600

    ⬆️ Qr Payment Method
    Type(s): UPDATE

--- config/pos.php
+++ config/pos.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'payment_method' => ['cod', 'bkash', 'online', 'others', 'payment_link','qr_code','advance_balance','emi'],
+    'payment_method' => ['cod', 'bkash', 'online', 'others', 'payment_link','qr_code','advance_balance','emi', 'qr_payment'],
     'minimum_order_amount_for_emi' => 5000,
     'payment_link_url' => env('SHEBA_PAYMENT_LINK_URL'),
     'payment_link_web_url' => env('SHEBA_PAYMENT_LINK_WEB_URL'),
